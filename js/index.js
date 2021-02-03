9e7db3ea7466ae532ae1f507ed7ab744cb97432e
Rodolfo <noctusm85@gmail.com>
2021-02-02 22:29:52 -0600

    Proyecto finalizado

--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const panels = document.querySelectorAll('.panel');
+
+panels.forEach(panel => {
+	panel.addEventListener('click', () => {
+		removeClasses();
+		panel.classList.add('active');
+	});
+});
+
+const removeClasses = () => {
+	panels.forEach(panel => panel.classList.remove('active'));
+};
