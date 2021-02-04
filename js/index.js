17eec59d8831cf1d7b4098c46dccb608dfe43d92
Rodolfo <noctusm85@gmail.com>
2021-02-04 15:15:58 -0600

    Proyecto day 5 Blurry loading finalizado

--- js/index.js
+++ js/index.js
@@ -1,1 +1,23 @@
 'use strict';
+const bg = document.querySelector('.bg');
+const loadingText = document.querySelector('.loading-text');
+
+let load = 0;
+
+//Ejecuta la funcion blurring cada 30 milisegundos
+let int = setInterval(blurring, 30);
+
+function blurring() {
+	load++;
+
+	if (load > 99) clearInterval(int);
+
+	loadingText.textContent = `${load}%`;
+	loadingText.style.opacity = scale(load, 0, 100, 1, 0);
+
+	bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`;
+}
+
+const scale = (num, in_min, in_max, out_min, out_max) => {
+	return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
+};
