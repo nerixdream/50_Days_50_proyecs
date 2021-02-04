ec0003f300dd2e9632927273583159e47b3eb5b0
Rodolfo <noctusm85@gmail.com>
2021-02-03 23:05:20 -0600

    Proyecto day3 finalizado

--- js/index.js
+++ js/index.js
@@ -1,1 +1,7 @@
 'use strict';
+const open = document.querySelector('#open');
+const close = document.querySelector('#close');
+const container = document.querySelector('.container');
+
+open.addEventListener('click', () => container.classList.add('show-nav'));
+close.addEventListener('click', () => container.classList.remove('show-nav'));
