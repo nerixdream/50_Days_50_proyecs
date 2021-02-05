f1deb6bb486565e6cb3e04a8c93176a9cd8689af
Rodolfo <noctusm85@gmail.com>
2021-02-05 16:23:46 -0600

    Proyecto day8 Form Wave Animation finalizado

--- js/index.js
+++ js/index.js
@@ -1,1 +1,9 @@
 'use strict';
+const labels = document.querySelectorAll('.form-control label');
+
+labels.forEach(label => {
+	label.innerHTML = label.textContent
+		.split('')
+		.map((letter, i) => `<span style="transition-delay: ${i * 50}ms">${letter}</span>`)
+		.join('');
+});
