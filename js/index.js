23dc64f7670bc813de7868e005ba566a7768cdf2
Rodolfo <noctusm85@gmail.com>
2021-02-06 14:29:12 -0600

    Projecto Day10 Dad Jokes Finalizado

--- js/index.js
+++ js/index.js
@@ -1,1 +1,21 @@
 'use strict';
+const jokeEl = document.getElementById('joke');
+const jokeBtn = document.getElementById('jokeBtn');
+
+const getJoke = async () => {
+	const url = 'https://icanhazdadjoke.com/';
+	const config = {
+		headers: {
+			Accept: 'application/json',
+		},
+	};
+
+	const respuesta = await fetch(url, config);
+	const { joke } = await respuesta.json();
+
+	jokeEl.innerHTML = joke;
+};
+
+getJoke();
+
+jokeBtn.addEventListener('click', getJoke);
