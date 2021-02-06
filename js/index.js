d52aec583327e1cee59ecec6f006b602eb402309
Rodolfo <noctusm85@gmail.com>
2021-02-06 13:38:27 -0600

    Proyecto Day9 Sound Board finalizado

--- js/index.js
+++ js/index.js
@@ -1,1 +1,25 @@
 'use strict';
+
+const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong'];
+
+sounds.forEach(sound => {
+	const btn = document.createElement('button');
+	btn.classList.add('btn');
+
+	btn.textContent = sound;
+
+	btn.addEventListener('click', () => {
+		stopSound();
+		document.getElementById(sound).play();
+	});
+
+	document.querySelector('#sounds').appendChild(btn);
+});
+
+function stopSound() {
+	sounds.forEach(sound => {
+		const song = document.getElementById(sound);
+		song.pause();
+		song.currentTime = 0;
+	});
+}
