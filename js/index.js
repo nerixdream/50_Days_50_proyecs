accca4f0e9e11189beb76ae855bb1592f1e2abe4
Rodolfo <noctusm85@gmail.com>
2021-02-07 21:52:47 -0600

    Proyecto Day12 FAQ finalizado

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const buttons = document.querySelectorAll('.faq-toggle');
+
+buttons.forEach(button => {
+	button.addEventListener('click', () => {
+		const parent = button.parentElement;
+		parent.classList.toggle('active');
+	});
+});
