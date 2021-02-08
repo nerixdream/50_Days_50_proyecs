7959e2213c3f91f8f0e79b8315eef0d5dca29651
Rodolfo <noctusm85@gmail.com>
2021-02-08 14:49:26 -0600

    Proyecto Day13 Random Choice Picker finalizado

--- js/index.js
+++ js/index.js
@@ -1,1 +1,75 @@
 'use strict';
+
+const tagsEl = document.getElementById('tags');
+const textarea = document.getElementById('textarea');
+
+textarea.focus();
+
+textarea.addEventListener('keyup', e => {
+	createTags(e.target.value);
+
+	if (e.key === 'Enter') {
+		setTimeout(() => {
+			e.target.value = '';
+		}, 10);
+		randomSelect();
+	}
+});
+
+//Función para crear los tags separados por coma
+function createTags(input) {
+	const tags = input
+		.split(',') //Divide por coma
+		.filter(tag => tag.trim() !== '') //Filtra los espacios vacios
+		.map(tag => tag.trim()); //Quita los espacios
+
+	tagsEl.innerHTML = '';
+
+	tags.forEach(tag => {
+		const tagEl = document.createElement('span');
+		tagEl.classList.add('tag');
+		tagEl.textContent = tag;
+
+		tagsEl.appendChild(tagEl);
+	});
+}
+
+function randomSelect() {
+	const seconds = 5;
+
+	const interval = setInterval(() => {
+		const randomTag = pickRandomTag();
+
+		hightLihgtTag(randomTag);
+
+		setTimeout(() => {
+			unHightLihgtTag(randomTag);
+		}, 100);
+	}, 100);
+
+	setTimeout(() => {
+		clearInterval(interval);
+
+		setTimeout(() => {
+			const randomTag = pickRandomTag();
+
+			hightLihgtTag(randomTag);
+		}, 100);
+	}, seconds * 1000);
+}
+
+//Devuelve un tag aleatorio
+function pickRandomTag() {
+	const tags = document.querySelectorAll('.tag');
+	return tags[Math.floor(Math.random() * tags.length)];
+}
+
+//Agrega la clase hightlight
+function hightLihgtTag(tag) {
+	tag.classList.add('hightlight');
+}
+
+//Quita la clase hightlight
+function unHightLihgtTag(tag) {
+	tag.classList.remove('hightlight');
+}
