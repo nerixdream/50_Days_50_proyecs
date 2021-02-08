5a5c5ed54d4e954ec8e1315a1ea93823535c48cc
Rodolfo <noctusm85@gmail.com>
2021-02-07 20:56:27 -0600

    Proyecto Day11 Event KeyCodes finalizado

--- js/index.js
+++ js/index.js
@@ -1,1 +1,21 @@
 'use strict';
+const insert = document.querySelector('#insert');
+
+document.addEventListener('keydown', e => {
+	const html = `
+        <div class="key">
+			${e.key === ' ' ? 'Space' : e.key}
+			<small>event.key</small>
+		</div>
+		<div class="key">
+			${e.keyCode}
+			<small>event.keyCode</small>
+			</div>
+		<div class="key">
+			${e.code}
+			<small>event.code</small>
+		</div>
+    `;
+
+	insert.innerHTML = html;
+});
