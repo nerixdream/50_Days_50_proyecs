7b72c9df98f4e56b23b2640dc805ca278f006192
Rodolfo <noctusm85@gmail.com>
2021-02-10 01:32:13 -0600

    Proyecto Day15 Increment Counter finalizado

--- js/index.js
+++ js/index.js
@@ -1,1 +1,26 @@
 'use strict';
+
+const counters = document.querySelectorAll('.counter');
+
+counters.forEach(counter => {
+	counter.textContent = '0';
+
+	const updateCounter = () => {
+		const target = +counter.getAttribute('data-target');
+		const c = +counter.textContent;
+
+		const increment = target / 300;
+
+		if (c < target) {
+			counter.textContent = `${Math.ceil(c + increment)}`;
+
+			setTimeout(() => {
+				updateCounter();
+			}, 1);
+		} else {
+			counter.textContent = target;
+		}
+	};
+
+	updateCounter();
+});
