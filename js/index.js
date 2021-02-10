de8465cebe6d1e192723c96708f0c02029330b83
Rodolfo <noctusm85@gmail.com>
2021-02-10 17:42:56 -0600

    Proyecto Day16 Drink Water finalizado

--- js/index.js
+++ js/index.js
@@ -1,1 +1,46 @@
 'use strict';
+
+const cups = document.querySelectorAll('.cup-small'),
+	liters = document.querySelector('#liters'),
+	percentage = document.querySelector('#percentage'),
+	remained = document.querySelector('#remained');
+
+updateBigCup();
+
+cups.forEach((cup, index) => {
+	cup.addEventListener('click', () => hightLightCups(index));
+});
+
+function hightLightCups(index) {
+	if (cups[index].classList.contains('full') && !cups[index].nextElementSibling.classList.contains('full')) {
+		index--;
+	}
+
+	cups.forEach((cup, idx) => {
+		idx <= index ? cup.classList.add('full') : cup.classList.remove('full');
+	});
+
+	updateBigCup();
+}
+
+function updateBigCup() {
+	const fullCups = document.querySelectorAll('.cup-small.full').length,
+		totalCups = cups.length;
+
+	if (fullCups === 0) {
+		percentage.style.visibility = 'hidden';
+		percentage.style.height = 0;
+	} else {
+		percentage.style.visibility = 'visible';
+		percentage.style.height = `${(fullCups / totalCups) * 33}rem`;
+		percentage.textContent = `${(fullCups / totalCups) * 100}%`;
+	}
+
+	if (fullCups === totalCups) {
+		remained.style.visibility = 'hidden';
+		remained.style.height = 0;
+	} else {
+		remained.style.visibility = 'visible';
+		liters.textContent = `${2 - (250 * fullCups) / 1000}`;
+	}
+}
