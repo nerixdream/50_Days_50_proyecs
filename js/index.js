ae6499e180acfb503eaec83ba2370ac657a3d058
Rodolfo <noctusm85@gmail.com>
2021-02-22 23:36:28 -0600

    Proyecto Day20 Button Ripple Effect finalizado

--- js/index.js
+++ js/index.js
@@ -1,1 +1,28 @@
 'use strict';
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach(button => {
+	button.addEventListener('click', function (e) {
+		//Variables del click en el cliente (navegador)
+		const x = e.clientX;
+		const y = e.clientY;
+
+		//Variables del click sobre la posición del botón
+		const buttonTop = e.target.offsetTop;
+		const buttonLeft = e.target.offsetLeft;
+
+		const xInside = x - buttonLeft;
+		const yInside = y - buttonTop;
+
+		const circle = document.createElement('span');
+		circle.classList.add('circle');
+		circle.style.top = yInside + 'px';
+		circle.style.left = xInside + 'px';
+
+		this.appendChild(circle);
+
+		setTimeout(() => {
+			circle.remove();
+		}, 500);
+	});
+});
