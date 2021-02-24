522dae23804a1e3df9269b6fd78b61f7a5fc49dd
Rodolfo <noctusm85@gmail.com>
2021-02-23 23:43:46 -0600

    Proyecto Day21 Drag N Drop finalizado

--- js/index.js
+++ js/index.js
@@ -1,1 +1,45 @@
 'use strict';
+
+const fill = document.querySelector('.fill'),
+	emties = document.querySelectorAll('.empty');
+
+fill.addEventListener('dragstart', dragStart);
+fill.addEventListener('dragend', dragEnd);
+
+for (const empty of emties) {
+	empty.addEventListener('dragover', dragOver);
+	empty.addEventListener('dragenter', dragEnter);
+	empty.addEventListener('dragleave', dragLeave);
+	empty.addEventListener('drop', dragDrop);
+}
+
+function dragStart() {
+	this.classList += ' hold';
+	setTimeout(() => {
+		this.classList = '';
+	}, 0);
+}
+
+function dragEnd() {
+	this.classList = 'fill';
+}
+
+//Previene su comportamiento por defecto del evento draggable
+//De esta manera podemos arrastrar a cualquier div
+function dragOver(e) {
+	e.preventDefault();
+}
+
+function dragEnter(e) {
+	e.preventDefault();
+	this.classList += ' hovered';
+}
+
+function dragLeave() {
+	this.classList = 'empty';
+}
+
+function dragDrop() {
+	this.classList = 'empty';
+	this.append(fill);
+}
