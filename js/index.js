fe2b919481effdb84d5490b3cdc307cf7a188de8
Rodolfo <noctusm85@gmail.com>
2021-02-25 17:34:00 -0600

    Proyecto Day25 Sticky Navigation finalizado

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const nav = document.querySelector('.nav');
+
+const fixNav = () => {
+	if (window.scrollY > nav.offsetHeight + 150) {
+		nav.classList.add('active');
+	} else {
+		nav.classList.remove('active');
+	}
+};
+
+window.addEventListener('scroll', fixNav);
