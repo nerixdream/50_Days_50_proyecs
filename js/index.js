af200f9f7a14b19a2dba5b50eb1403e6652e4e39
Rodolfo <noctusm85@gmail.com>
2021-03-01 00:32:29 -0600

    Proyecto Day27 Toast Notification finalizado

--- js/index.js
+++ js/index.js
@@ -1,1 +1,24 @@
 'use strict';
+
+const button = document.querySelector('#button'),
+	toastsDiv = document.querySelector('#toasts');
+
+const messages = ['Mensaje uno', 'Mensaje dos', 'Mensaje tres', 'Mensaje cuatro'];
+
+button.addEventListener('click', createNotification);
+
+function createNotification() {
+	const noti = document.createElement('div');
+	noti.classList.add('toast');
+	noti.textContent = randomMessage();
+
+	toastsDiv.appendChild(noti);
+
+	setTimeout(() => {
+		noti.remove();
+	}, 2000);
+}
+
+function randomMessage() {
+	return messages[Math.floor(Math.random() * messages.length)];
+}
