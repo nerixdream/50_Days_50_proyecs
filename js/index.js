427f014fcc606bf51b29f753e813cfac6f9e82de
Rodolfo <noctusm85@gmail.com>
2021-03-03 13:32:17 -0600

    Proyecto Day30 Auto Text Effect finalizado

--- js/index.js
+++ js/index.js
@@ -1,1 +1,22 @@
 'use strict';
+const textEl = document.querySelector('#text'),
+	speedEl = document.querySelector('#speed'),
+	text = 'We Love Programming!';
+let idx = 1;
+let speed = 300 / speedEl.value;
+
+const writeText = () => {
+	textEl.textContent = text.slice(0, idx);
+
+	idx++;
+
+	if (idx > text.length) {
+		idx = 1;
+	}
+
+	setTimeout(writeText, speed);
+};
+
+writeText();
+
+speedEl.addEventListener('input', e => (speed = 300 / e.target.value));
