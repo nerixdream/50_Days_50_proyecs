4d0ed755bffb661a7252acf41b87b914c1639573
Rodolfo <noctusm85@gmail.com>
2021-03-13 23:20:59 -0600

    Proyecto Day38 Mobile Tab Navigation finalizado

--- js/index.js
+++ js/index.js
@@ -1,1 +1,22 @@
 'use strict';
+
+const contents = document.querySelectorAll('.content'); //Selecciona todas las imagenes
+const listItems = document.querySelectorAll('nav ul li'); //Selecciona todos los enlaces
+
+listItems.forEach((item, idx) => {
+	item.addEventListener('click', () => {
+		hideAllContent(); //Quita la clase de show
+		hideAllItems(); //Quita la clase de active
+
+		item.classList.add('active'); //Añade la clase active
+		contents[idx].classList.add('show'); //Añade la clase active
+	});
+});
+
+function hideAllContent() {
+	contents.forEach(content => content.classList.remove('show'));
+}
+
+function hideAllItems() {
+	listItems.forEach(item => item.classList.remove('active'));
+}
