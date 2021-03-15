7dd382ee86fbced4bd32106d4759ee5d2970feac
Rodolfo <noctusm85@gmail.com>
2021-03-15 13:07:53 -0600

    Proyecto Day40 3D Boxes Background finalizado

--- js/index.js
+++ js/index.js
@@ -1,1 +1,19 @@
 'use strict';
+const boxes = document.getElementById('boxes');
+const btn = document.getElementById('btn');
+
+btn.addEventListener('click', () => boxes.classList.toggle('big'));
+
+const createBoxes = () => {
+	for (let i = 0; i < 4; i++) {
+		for (let j = 0; j < 4; j++) {
+			const box = document.createElement('div');
+			box.classList.add('box');
+			box.style.backgroundPosition = `${-j * 12.5}rem ${-i * 12.5}rem`;
+
+			boxes.appendChild(box);
+		}
+	}
+};
+
+createBoxes();
